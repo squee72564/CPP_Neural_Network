7ea52d01df384e84bc8e96e8e7269b8e56b61295
squee72564 <rodriguez.al183@gmail.com>
2024-05-02 11:34:08 -0700

    Added feed forward for NeuralNet

--- src/NeuralNet.cpp
+++ src/NeuralNet.cpp
@@ -18,7 +18,9 @@ NeuralNet::NeuralNet(const std::vector<uint32_t> &topology)
             );
 
         // Randomize weights in each connection for each neuron
-        for (auto& neuron : layers_[i]) {
+        for (std::size_t n = 0; i < layers_[i].size(); ++n) {
+            Neuron &neuron = layers_[i][n];
+            neuron.index = n;
             for (auto& connection : neuron.output_weights_) {
                 connection.weight_ = get_random_weight();
             }
@@ -28,10 +30,22 @@ NeuralNet::NeuralNet(const std::vector<uint32_t> &topology)
 
 void NeuralNet::FeedForward(const std::vector<double> &input_values)
 {
-    for (const double d : input_values) {
-        std::cout << d << " ";
+    // assert inputs == input neruons - bias nueron
+    assert(input_values.size() == layers_[0].size() - 1);
+
+    // Assign (latch) the input values into the input neurons
+    for (std::size_t i = 0; i < input_values.size(); ++i) {
+        layers_[0][i].output_value_ = input_values[i];
     }
-    std::cout << "\n";
+
+    // Forward Propegate
+    for (std::size_t layer_idx = 1; layer_idx < layers_.size(); ++layer_idx) {
+        Layer &prev_layer = layers_[layer_idx-1];
+
+        for (std::size_t neuron_idx = 0; neuron_idx < layers_[layer_idx].size()-1; ++neuron_idx) {
+            layers_[layer_idx][neuron_idx].FeedForward(prev_layer);
+        }
+    }
 }
 
 void NeuralNet::BackPropagation(const std::vector<double> &target_values)
