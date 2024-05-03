d99789ab7bc5802c35720ec1166b02f0070b60ab
Alex Rodriguez <rodriguez.al183@gmail.com>
2024-05-02 22:48:07 -0700

    Added different activation functions that work with new topology struct

--- include/Neuron.hpp
+++ include/Neuron.hpp
@@ -4,9 +4,9 @@
 #include <vector>
 #include <cmath>
 #include <cstdint>
+#include <functional>
 
 class Neuron;
-
 typedef std::vector<Neuron> Layer;
 
 struct Connection {
@@ -19,17 +19,35 @@ public:
 
 class Neuron {
 public:
+    enum ActivationFunction {
+        TanH = 0,
+        Sigmoid,
+        Relu,
+        SoftMax,
+    };
+
+
     std::vector<Connection> output_weights_;
     double output_value_;
     double gradient_;
     size_t neuron_index_;
     static double eta_;
     static double alpha_;
+    
+    
 
-    Neuron(uint32_t num_outputs);
+    Neuron(uint32_t num_outputs, Neuron::ActivationFunction f);
 
-    static double ActivationFunction(double x);
-    static double ActivationFunctionDerivative(double x);
+    static double _TanH(double x);
+    static double _TanHDerivative(double x);
+    static double _Sigmoid(double x);
+    static double _SigmoidDerivative(double x);
+    static double _Relu(double x);
+    static double _ReluDerivative(double x);
+    ActivationFunction activation_function_;
+
+    double ApplyActivationFunction(double x);
+    double ApplyActivationFunctionDerivative(double x);
     double SumDOW(const Layer &next_layer) const;
     void FeedForward(const Layer& prev_layer);
     void CalculateOuputGradients(const double target_value);
